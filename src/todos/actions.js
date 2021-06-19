7a12be06c6265a3d450c26c77f2a02ed3bc51263
Charles <1309571292@qq.com>
2021-06-19 22:00:44 +0800

    为TODO模块添加action

---
+++ src/todos/actions.js
@@ -0,0 +1,19 @@
+import { ADD_TODO, TOGGLE_TODO, REMOVE_TODO } from './ActionTypes';
+let nextTodoId = 0;
+
+export const addTodo = (text) => ({
+  type: ADD_TODO,
+  text,
+  completed: false,
+  id: nextTodoId + 1,
+});
+
+export const toggleTodo = (id) => ({
+  type: TOGGLE_TODO,
+  id,
+});
+
+export const removeTodo = (id) => ({
+  type: REMOVE_TODO,
+  id,
+});
