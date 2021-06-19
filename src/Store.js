4183629613a876440f3600efb4230eaccd89e32e
Charles <1309571292@qq.com>
2021-06-19 21:44:58 +0800

    组织代码

---
+++ src/Store.js
@@ -0,0 +1,9 @@
+import { createStore } from 'redux';
+
+const store = {
+  getState: () => {},
+  dispatch: () => {},
+  subscribe: () => {},
+};
+
+export default store;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import TodoApp from './TodoApp';
+import { Provider } from 'react-redux';
+import store from './Store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <TodoApp />
+  </Provider>,
+  document.getElementById('root'),
+);
