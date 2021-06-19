5a751c7fbe9f19946b1bd6524105a7b4ad543654
Charles <1309571292@qq.com>
2021-06-19 22:33:36 +0800

    定义Filter模块的reducer

--- src/filter/reducer.js
+++ src/filter/reducer.js
@@ -1,1 +1,13 @@
-export default (state = [], action) => {};
+import { SET_FILTER } from './ActionTypes';
+import { FilterTypes } from '../constants';
+
+const reducer = (state = FilterTypes.ALL, action) => {
+  const { type, filter } = action;
+  switch (type) {
+    case SET_FILTER:
+      return filter;
+    default:
+      return state;
+  }
+};
+export default reducer;
