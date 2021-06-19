782ecfbb18e6b5c581cd926e3ddd5b3b73fd881c
Charles <1309571292@qq.com>
2021-06-19 22:23:33 +0800

    为TODO模块添加reducer

--- src/todos/reducer.js
+++ src/todos/reducer.js
@@ -1,1 +1,30 @@
-export default (state = [], action) => {};
+import { ADD_TODO, TOGGLE_TODO, REMOVE_TODO } from './ActionTypes';
+
+const reducer = (state = [], action) => {
+  const { type, id, text, completed } = action || {};
+  switch (type) {
+    case ADD_TODO:
+      return [
+        {
+          id,
+          text,
+          completed,
+        },
+        ...state,
+      ];
+    case TOGGLE_TODO:
+      return state.map((item) => {
+        if (item.id === id) {
+          return { ...item, completed: !item.completed };
+        } else {
+          return item;
+        }
+      });
+    case REMOVE_TODO:
+      return state.filter((item) => item.id !== id);
+    default:
+      return state;
+  }
+};
+
+export default reducer;
